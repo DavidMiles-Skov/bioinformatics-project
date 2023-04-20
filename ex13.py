7e2e0acd86e0f8a9b07aea6d9663a4487cb748ed
RolfLarsen1 <129761664+RolfLarsen1@users.noreply.github.com>
2023-04-20 11:51:00 +0200

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from read_data import getData
+
+"""
+Attempting to solve exercise 13
+Tall is considered above 184.4 cm for men and above 170.2 for women
+Notes:
+Same as in ex12, Ive realied the height for women on average is very tall compared to denmark statistics
+Maybe we should not differentiate between them in that case
+Formatting needs with with the "main()"
+Output can be cleaned up
+
+"""
+
+def children_heights():
+	"""Function for finding the percentage of tall children with 2 tall parents"""
+	girls, boys = [], []
+	tall, normal, short = 0,0,0
+
+	people = getData()
+
+	for cpr, person in people.items():
+
+		parents = person.Parents
+
+		if parents != []:
+			#Making it so the father is always defined as p1
+			if (int(people[parents[0]].CPR[9:11]) % 2) == 0:
+				p1 = people[parents[1]]
+				p2 = people[parents[0]]
+			elif (int(people[parents[0]].CPR[9:11]) % 2) != 0:
+				p1 = people[parents[0]]
+				p2 = people[parents[1]]
+
+			#If both parenst are tall
+			if p1.Height > 184.4 and p2.Height > 170.2:
+				if (int(person.CPR[9:11]) % 2) == 0: girls.append(person.Height)                  #girls
+
+				elif (int(person.CPR[9:11]) % 2) != 0: boys.append(person.Height)                 #boys
+
+	for boy in boys:
+		if boy > 184.4: tall += 1
+		elif boy <= 184.4 and boy >= 178.4: normal += 1
+		if boy < 178.4: short += 1
+
+	for girl in girls:
+		if girl > 170.2: tall += 1
+		elif girl <= 170.2 and girl >= 164.2: normal += 1
+		if girl < 164.2: short += 1
+
+	total = tall + normal + short
+	return ((tall/total)*100, (normal/total)*100, (short/total)*100)
+
+print(children_heights())
