5f81d8e4514d260bde135f0e8d42fdd3c25d3dc9
RolfLarsen1 <129761664+RolfLarsen1@users.noreply.github.com>
2023-04-20 21:30:09 +0200

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -7,7 +7,7 @@ Using the inheritance chart from https://canadiancrc.com/paternity_determination
 Notes:
 As far as i could read the rhesus +/- system is inherited seperately from the ABO-system, so I've only based it on the ABO-system
 Directory needs fixing
-Output needs fixing
+Output can be improved, atm its just a list of the childrens CPR numbers
 
 """
 
